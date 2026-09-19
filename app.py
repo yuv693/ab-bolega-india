d69d175bb9e83695a9d1dbf0011a3d838b4dd20c
check_vehicle_loan <malekyunus44@gmail.com>
2026-09-19 19:06:01 +0530

    Update app to run on configurable port

--- app.py
+++ app.py
@@ -21,5 +21,9 @@ def book_ticket():
         "fare": "₹1150"
     })
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
